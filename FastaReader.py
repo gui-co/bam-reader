97d572e4df2b90a8b6df990e3ffbf1062e4233a0
Guillaume Communie <gui-co@posteo.net>
2021-02-25 22:15:23 +0100

    New class to read fasta files

---
+++ FastaReader.py
@@ -0,0 +1,36 @@
+class FastaReader:
+    _f = None
+
+    def __init__(self, filename):
+        try:
+            self._f = open(filename, "r")
+        except Exception as ex:
+            raise Exception("[Error] unable to open fasta file: " + str(ex))
+
+    def __del__(self):
+        self._f.close()
+
+    def getSequence(self, sequenceName):
+        while True:
+            line = self._f.readline()
+            if not line:
+                raise Exception("[Error] sequence {} not found in fasta file"
+                                .format(sequenceName))
+            if line[0] == '>':
+                name = line.split(' ')[0]
+                if name[1:] == sequenceName or name == sequenceName:
+                    break
+
+        sequence = list()
+        while True:
+            line = self._f.readline()
+            for i in line:
+                if i in "ACTGN":
+                    sequence.append(i)
+                elif i in "\r\n\t ":
+                    continue
+                elif i == '>':
+                    return sequence
+                else:
+                    raise Exception("[Error] sequence {} is corrupted")
+
